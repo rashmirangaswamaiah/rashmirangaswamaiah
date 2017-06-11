66c097e9312b78507d34635f4612d27ffe4c78fb
rashmirangaswamaiah <you@example.com>
2017-06-11 20:23:32 +0200

    added all files

---
+++ MainScreenJavaScript.js
@@ -0,0 +1,31 @@
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n)
+}
+function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        if(n > slides.length){
+          slideIndex =1
+        }
+        if (n < 1) {
+          slideIndex = slides.length
+        }
+        for ( i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+        }
+        slides[slideIndex-1].style.display = "block";
+  }
+
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if(x.className == "topnav"){
+        x.className += " responsive";
+    }else {
+      x.className = "topnav";
+    }
+  }
