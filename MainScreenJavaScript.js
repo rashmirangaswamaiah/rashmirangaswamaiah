69afe2d21d22af538f444f3b3f731f9a83fcb51c
rashmirangaswamaiah <you@example.com>
2017-06-22 21:52:32 +0200

    added code for the dots

--- MainScreenJavaScript.js
+++ MainScreenJavaScript.js
@@ -9,9 +9,9 @@ function currentSlide(n) {
 function showSlides(n) {
         var i;
         var slides = document.getElementsByClassName("mySlides");
-        var  dots = document.getElementsByClassName("dot");
+        var dots = document.getElementsByClassName("dot");
         if(n > slides.length){
-          slideIndex =1
+          slideIndex = 1
         }
         if (n < 1) {
           slideIndex = slides.length
@@ -19,13 +19,13 @@ function showSlides(n) {
         for ( i = 0; i < slides.length; i++) {
           slides[i].style.display = "none";
         }
-        for ( i = 0; i < dots.length++) {
-          dots[i].className = dots[i].className.replace(" active","");
+        for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
         }
-
         slides[slideIndex-1].style.display = "block";
         dots[slideIndex-1].className += " active";
   }
+
 
   function myFunction() {
     var x = document.getElementById("myTopnav");
